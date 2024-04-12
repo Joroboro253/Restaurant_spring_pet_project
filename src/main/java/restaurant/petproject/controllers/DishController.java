041b11217bc94f19c12b0ee388a791fd710f71e5
Vitaliy Lyashchenko <69591510+Joroboro253@users.noreply.github.com>
2024-04-12 23:16:37 +0300

    Details div with right

--- src/main/java/restaurant/petproject/controllers/DishController.java
+++ src/main/java/restaurant/petproject/controllers/DishController.java
@@ -1,45 +1,29 @@
 package restaurant.petproject.controllers;
 
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 import restaurant.petproject.entity.Dish;
 import restaurant.petproject.entity.Image;
-import restaurant.petproject.entity.User;
 import restaurant.petproject.repository.DishRepository;
-import restaurant.petproject.service.ImageService;
 import restaurant.petproject.service.impl.DishServiceImpl;
 import restaurant.petproject.service.impl.ImageServiceImpl;
 import restaurant.petproject.service.impl.ShopServiceImpl;
 import restaurant.petproject.service.impl.UserServiceImpl;
 
-import javax.sql.rowset.serial.SerialBlob;
-import javax.sql.rowset.serial.SerialException;
-import java.io.IOException;
 import java.security.Principal;
-import java.sql.Blob;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Base64;
-import java.util.List;
-import java.util.Optional;
 
 @Controller
-//@RequestMapping("/User")
 public class DishController {
     private final ShopServiceImpl shopService;
-    private DishRepository dishRepository;
+    private final DishRepository dishRepository;
     private final DishServiceImpl dishService;
-    private ImageServiceImpl imageService;
+    private final ImageServiceImpl imageService;
     private UserServiceImpl userService;
     @Autowired
     public DishController(ShopServiceImpl shopService, DishRepository dishRepository, DishServiceImpl dishService, ImageServiceImpl imageService, UserServiceImpl userService) {
@@ -51,30 +35,20 @@ public class DishController {
     }
 
     @GetMapping("/menu")
-    public String menuMain(Principal principal, Model model){
+    public String menuMain(Model model){
         Iterable<Dish> dishes = dishRepository.findAll();
         model.addAttribute("dishes", dishes);
 
-        String Email = SecurityContextHolder.getContext().getAuthentication().getName();
-        User user = userService.findByEmail(Email);
-
-        if (user != null) {
-            model.addAttribute("user", user);
-            model.addAttribute("shop", shopService.getShoppingCartByUser(user));
-        } else {
-            model.addAttribute("user", new User());
-            model.addAttribute("shop", null);
-        }
         return "menu";
     }
 
     @GetMapping("/authorization")
-    public String authorizationGet(Model model) {
+    public String authorizationGet() {
         return "authorization";
     }
 
     @GetMapping("/display")
-    public ResponseEntity<byte[]> displayImage(@RequestParam("id") long id) throws IOException, SQLException
+    public ResponseEntity<byte[]> displayImage(@RequestParam("id") long id) throws SQLException
     {
         Image image = imageService.viewById(id);
         byte [] imageBytes = null;
--- src/main/java/restaurant/petproject/controllers/GlobalControllerAdvice.java
+++ src/main/java/restaurant/petproject/controllers/GlobalControllerAdvice.java
@@ -1,11 +1,12 @@
 package restaurant.petproject.controllers;
 
-import jakarta.validation.constraints.Email;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import restaurant.petproject.entity.User;
-import restaurant.petproject.service.UserService;
+import restaurant.petproject.service.impl.ShopServiceImpl;
 import restaurant.petproject.service.impl.UserServiceImpl;
 
 // Благодаря данной аннотации код автоматически применится к контроллерам
@@ -13,8 +14,24 @@ import restaurant.petproject.service.impl.UserServiceImpl;
 public class GlobalControllerAdvice {
     @Autowired
     private UserServiceImpl userService;
+    @Autowired
+    private ShopServiceImpl shopService;
+
     @ModelAttribute("users")
-    public User globalUser() {
+    public User globalUser(Model model) {
+        String Email = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userService.findByEmail(Email);
+
+        if (user != null) {
+            model.addAttribute("user", user);
+            model.addAttribute("shop", shopService.getShoppingCartByUser(user));
+        } else {
+            model.addAttribute("user", new User());
+            model.addAttribute("shop", null);
+        }
+
         return userService.getCurrentUser();
+
+
     }
 }

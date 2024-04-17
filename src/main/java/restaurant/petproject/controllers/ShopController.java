c5d1bfe819b407a4ba09900a58b5733499483002
Vitaliy Lyashchenko <69591510+Joroboro253@users.noreply.github.com>
2024-04-17 09:18:18 +0300

    Creating a cart

--- src/main/java/restaurant/petproject/controllers/ShopController.java
+++ src/main/java/restaurant/petproject/controllers/ShopController.java
@@ -1,11 +1,18 @@
 package restaurant.petproject.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import restaurant.petproject.entity.ShoppingCart;
+import restaurant.petproject.entity.User;
 import restaurant.petproject.service.impl.DishServiceImpl;
 import restaurant.petproject.service.impl.ShopServiceImpl;
 import restaurant.petproject.service.impl.UserServiceImpl;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("User")
@@ -20,5 +27,39 @@ public class ShopController {
         this.dishService = dishService;
     }
 
+    @GetMapping("/cart")
+    public String cartView(Model model, @RequestParam("id") Optional<Long> id, Authentication auth){
+        if(id.isPresent()){
+            model.addAttribute("shop", shopService.getShoppingCartById(id.get()));
+        }else{
+            model.addAttribute("shop", shopService.creatEmptyCart((User) auth.getPrincipal()));
+        }
 
+        model.addAttribute("user", userService.findByEmail(auth.getName()));
+        return "cart";
+    }
+
+    @PostMapping("/add-cart")
+    public String addToCart(Model model, Authentication auth, @RequestParam("id") Long id,
+                            @RequestParam("quantity") Integer quantity ) {
+        shopService.addItem(id,quantity);
+        return "redirect:/User/index";
+    }
+    @GetMapping("/remove-cartItem/{id}")
+    public String addToCart(Model model, Authentication auth, @PathVariable("id") Long id) {
+        ShoppingCart shop = shopService.removeItem((User) auth.getPrincipal(),id);
+        model.addAttribute("shop", shop);
+        model.addAttribute("user", userService.findByEmail(((User) ((User) auth.getPrincipal())).getUsername()));
+        return "cart";
+    }
+
+    @GetMapping("/update-cartItem")
+    public String updateItemQuantityCart(Model model, Authentication auth, @RequestParam("id") Long id,
+                                         @RequestParam("quantity") Integer quantity ) {
+        ShoppingCart shop = shopService.updateItemQuantity((User) auth.getPrincipal(),id,quantity);
+        model.addAttribute("shop", shop);
+        System.out.println( ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).toString());
+        model.addAttribute("user", userService.findByEmail(((User) ((User) auth.getPrincipal())).getUsername()));
+        return "cart";
+    }
 }
